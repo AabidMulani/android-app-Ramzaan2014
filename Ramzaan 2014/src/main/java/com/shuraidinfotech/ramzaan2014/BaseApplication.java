da0e2066a3e2c23d2ab545ebc96665a66b6c29d4
Aabid Mulani <aabidmulani4u@gmail.com>
2014-09-02 10:04:28 +0530

    Code for Auto profiler

--- Ramzaan 2014/src/main/java/com/shuraidinfotech/ramzaan2014/BaseApplication.java
+++ Ramzaan 2014/src/main/java/com/shuraidinfotech/ramzaan2014/BaseApplication.java
@@ -1,7 +1,14 @@
 package com.shuraidinfotech.ramzaan2014;
 
+import com.orm.SugarApp;
+
 /**
  * Created by AABID on 18/5/14.
  */
-public class BaseApplication {
+public class BaseApplication extends SugarApp {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
 }
--- Ramzaan 2014/src/main/java/com/shuraidinfotech/ramzaan2014/utils/Logger.java
+++ Ramzaan 2014/src/main/java/com/shuraidinfotech/ramzaan2014/utils/Logger.java
@@ -4,76 +4,35 @@ import android.util.Log;
 
 public class Logger {
 
-	private static final boolean ENABLE_LOG = false;
-
-	private static final boolean DEBUG = true & ENABLE_LOG;
-
-	private static final boolean VERBOSE = true & ENABLE_LOG;
-
-	private static final boolean TEMP = true & ENABLE_LOG;
-
-	private static final boolean WARNING = true & ENABLE_LOG;
-
-	private static final boolean INFO = true & ENABLE_LOG;
-
-	private static final boolean ERROR = true & ENABLE_LOG;
-
-	public static void obvious(String tag, String msg) {
-		if (DEBUG) {
-			msg = "*********************************\n" + msg + "\n*********************************";
-			Log.d(tag, msg);
-		}
-	}
+	private static final boolean DO_LOG = true;
 
 	public static void d(String tag, String msg) {
-		if (DEBUG)
+		if (DO_LOG)
 			Log.d(tag, msg);
-	}
-
-	public static void d(boolean bool, String tag, String msg) {
-		if (TEMP & bool) {
-
-			Log.d(tag, msg);
-		}
 	}
 
 	public static void i(String tag, String msg) {
-		if (INFO)
+		if (DO_LOG)
 			Log.i(tag, msg);
 	}
 
 	public static void e(String tag, String msg) {
-		if (ERROR)
+		if (DO_LOG)
 			Log.e(tag, msg);
-	}
-
-	public static void e(boolean bool, String tag, String msg) {
-		if (TEMP & bool)
-			Log.e(tag, msg);
-	}
-
-	public static void e(String tag, String message, Throwable throwable) {
-		Log.e(tag, message, throwable);
-		throwable.printStackTrace();
 	}
 
 	public static void v(String tag, String msg) {
-		if (VERBOSE)
+		if (DO_LOG)
 			Log.v(tag, msg);
 	}
 
 	public static void w(String tag, String msg) {
-		if (WARNING)
+		if (DO_LOG)
 			Log.w(tag, msg);
 	}
 
 	public static String getStackTraceString(Exception e) {
 		return Log.getStackTraceString(e);
-	}
-
-	public static void w(String tag, String msg, Exception e) {
-		if (WARNING)
-			Log.w(tag, msg, e);
 	}
 
 }
